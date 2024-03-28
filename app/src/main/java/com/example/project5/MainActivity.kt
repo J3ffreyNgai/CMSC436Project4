c844979307baac4f3e060befad9ca6e5ed9798c7
Jeffrey Ngai <Jeffreyngai13579@gmail.com>
2024-03-28 17:45:21 -0400

    ADded GameTimerTask and bar movement

--- app/src/main/java/com/example/project5/MainActivity.kt
+++ app/src/main/java/com/example/project5/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.project5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.GestureDetector
 import android.view.MotionEvent
+import java.util.Timer
 
 class MainActivity : AppCompatActivity() {
     private lateinit var gameView: GameView
@@ -25,16 +27,40 @@ class MainActivity : AppCompatActivity() {
         gameView = GameView(this, width, height - statusBarHeight)
         setContentView(gameView)
         pong = gameView.getPong()
+
+        var th : TouchHandler = TouchHandler()
+        detector = GestureDetector( this, th )
+        detector.setOnDoubleTapListener( th )
+
+        var gameTimer : Timer = Timer( )
+        var task : GameTimerTask = GameTimerTask( this )
+        gameTimer.schedule( task, 0L, 100L )
     }
 
+    fun updateView( ) {
+        gameView.postInvalidate()
+    }
+    fun updateBar(e : MotionEvent) {
+        var x : Float = e.x
+        pong.updateBarPosition(x)
+    }
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         if( event != null )
             detector.onTouchEvent( event )
         return super.onTouchEvent(event)
     }
 
-    inner class TouchHandler : GestureDetector.SimpleOnGestureListener( ) {
-
+    inner class TouchHandler : GestureDetector.SimpleOnGestureListener() {
+        override fun onScroll(
+            e1: MotionEvent?,
+            e2: MotionEvent,
+            distanceX: Float,
+            distanceY: Float
+        ): Boolean {
+            Log.w("Main" , "Scroll")
+            updateBar(e2)
+            return super.onScroll(e1, e2, distanceX, distanceY)
+        }
     }
 
 }
